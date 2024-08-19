4377ae3df4a0ce5cb0a1673b6e20b08237925036
Jarten <Artemy2MA@outlook.com>
2024-08-19 13:26:50 -0700

    trying to fix boulder

--- boulder/boulder.gd
+++ boulder/boulder.gd
@@ -7,6 +7,10 @@ extends Node2D
 var rolling = false
 var current_frame: int = 0
 
+var falling_from: Vector2
+var fall_progress: float = 0
+var falling: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if AUTO_ROLL:
@@ -14,14 +18,25 @@ func _ready() -> void:
 	
 func on_collision(direction: Vector2):
 	if size <= 2:
-		DIRECTION = direction
+		DIRECTION = direction * -1
 		rolling = true
 
 func _physics_process(delta: float) -> void:
 	size = $Area2D/ScaleableSprite.current_tile
 
+	if falling:
+		fall_progress += delta
+		var falling_to = falling_from
+		falling_to.y += 64 * 4
+		position = falling_from.lerp(falling_to, pow(fall_progress, 2))
+		
+		if fall_progress >= 1:
+			queue_free()
 
 func tick():
+	if falling:
+		return
+
 	if rolling:
 		current_frame += 1
 		if current_frame > 2:
@@ -38,18 +53,20 @@ func tick():
 	$Area2D/ScaleableSprite.get_current_sprite().frame_coords.y = current_frame 
 
 	if get_tile_data(TILEMAP_MANAGER.FLOOR_LAYER) == null:
-		rotation
-		queue_free()
+		fall()
 
 func _on_area_2d_area_entered(area:Area2D) -> void:
 	if area is CarInternal:
 		on_collision(area.DIRECTION)
 
-
 func _on_global_tick_timeout() -> void:
 	tick()
 
 func get_tile_data(tile_map_layer: TileMapLayer) -> TileData:
-	var map_position = tile_map_layer.local_to_map($"..".position / tile_map_layer.scale.x)
-	print(map_position)
+	var map_position = tile_map_layer.local_to_map(position / tile_map_layer.scale.x)
 	return tile_map_layer.get_cell_tile_data(map_position)
+
+func fall():
+	falling = true
+	position += DIRECTION.normalized() * 64
+	falling_from = position
--- scripts/turn_tile_internal.gd
+++ scripts/turn_tile_internal.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var TURN_COUNTERCLOCKWISE
+var TURN_COUNTERCLOCKWISE: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,5 +11,5 @@ func _ready() -> void:
 
 func _on_area_entered(area:Area2D) -> void:
 	if area.has_meta("Tag") and area.get_meta("Tag") != "Car":
-		area.rotate(deg_to_rad(90) * sign(TURN_COUNTERCLOCKWISE - 0.5))
+		area.rotate(deg_to_rad(90) * sign(float(TURN_COUNTERCLOCKWISE) - 0.5))
 		get_parent().on_use()
