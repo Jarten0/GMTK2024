12fc98c48eb2a17d3d803bd8dd7c82d1a74bbe35
Jarten <Artemy2MA@outlook.com>
2024-08-20 00:47:33 -0700

    add dialog

---
+++ dialog.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+var lines = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	lines[0] = "one"
+	lines[0] = "one"
+	lines[0] = "one"
+	lines[0] = "one"
+	
